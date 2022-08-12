8f257c308fb5c4725fa213f4d93d29e9b7829206
guntur82 <gunturprakasairwan@gmail.com>
2022-08-12 17:41:33 +0700

    crud menu_makanan

--- controller/MakananController.js
+++ controller/MakananController.js
@@ -1,11 +1,12 @@
 const { makanan } = require('../models');
+const fs = require('fs');
+
 class MakananController {
   static async getData(req, res) {
     try {
       let result = await makanan.findAll({
         order: [['id', 'asc']],
       });
-      // res.json(result);
       res.render('staff/makanan');
     } catch (err) {
       res.json(err);
@@ -14,33 +15,79 @@ class MakananController {
 
   static async readPage(req, res) {
     try {
-      res.render('staff/menu_makanan');
+      let result = await makanan.findAll({
+        order: [['id', 'asc']],
+      });
+      res.render('staff/menu_makanan', { result: result });
     } catch (error) {
       res.json(error);
     }
   }
 
-  static async createPage(req, res) {}
+  static async createPage(req, res) {
+    try {
+      res.render('staff/menu_makanan/actionindex.ejs', { result: null });
+    } catch (error) {
+      res.json(error);
+    }
+  }
 
   static async create(req, res) {
     try {
-      const { name, harga, foto } = req.body;
+      const { name, harga } = req.body;
+      let foto = '';
+      if (req.files) {
+        const file = req.files.foto;
+        let date = new Date().getTime().toString();
+        foto = date + ' - ' + file.name;
+        if (!fs.existsSync(`${__dirname}/../public/uploads/`)) {
+          fs.mkdirSync(`${__dirname}/../public/uploads/`);
+        }
+        file.mv(`${__dirname}/../public/uploads/${foto}`);
+      }
       let result = await makanan.create({
         name,
         harga,
         foto,
       });
-      res.json(result);
+      res.redirect('/makanan/read');
     } catch (err) {
       res.json(err);
     }
   }
 
-  static async updatePage(req, res) {}
+  static async updatePage(req, res) {
+    try {
+      const id = req.params.id;
+      let result = await makanan.findByPk(id);
+      res.render('staff/menu_makanan/actionindex.ejs', { result: result });
+    } catch (error) {
+      res.json(error);
+    }
+  }
   static async update(req, res) {
     try {
       const id = req.params.id;
-      const { name, harga, foto } = req.body;
+      const { name, harga } = req.body;
+      let foto = '';
+      let old = await makanan.findByPk(id);
+      if (req.files) {
+        const file = req.files.foto;
+        let date = new Date().getTime().toString();
+        foto = date + ' - ' + file.name;
+        if (!fs.existsSync(`${__dirname}/../public/uploads/`)) {
+          fs.mkdirSync(`${__dirname}/../public/uploads/`);
+        }
+        if (fs.existsSync(`${__dirname}/../public/uploads/${old.foto}`)) {
+          fs.unlink(`${__dirname}/../public/uploads/${old.foto}`, (err) => {
+            if (err) throw err;
+            console.log('file has been deleted');
+          });
+        }
+        file.mv(`${__dirname}/../public/uploads/${foto}`);
+      } else {
+        foto = old.foto;
+      }
       let result = await makanan.update(
         {
           name,
@@ -51,18 +98,28 @@ class MakananController {
           where: { id },
         }
       );
-      res.json(result);
+      res.redirect('/makanan/read');
     } catch (error) {
-      res.json(error);
+      res.json('err = ' + error);
     }
   }
   static async delete(req, res) {
     try {
       const id = req.params.id;
+      let old = await makanan.findByPk(id);
+      if (!fs.existsSync(`${__dirname}/../public/uploads/`)) {
+        fs.mkdirSync(`${__dirname}/../public/uploads/`);
+      }
+      if (fs.existsSync(`${__dirname}/../public/uploads/${old.foto}`)) {
+        fs.unlink(`${__dirname}/../public/uploads/${old.foto}`, (err) => {
+          if (err) throw err;
+          console.log('file has been deleted');
+        });
+      }
       let result = await makanan.destroy({
         where: { id },
       });
-      res.json(result);
+      res.redirect('/makanan/read');
     } catch (error) {
       res.json(error);
     }
